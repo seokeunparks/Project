f37cfc9d988253a625149b31a2c25d4200973feb
seokeunparks <pppse620@gmail.com>
2023-10-25 19:11:53 +0900

    DB Address

---
+++ NowBoard.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class NowBoard {
+//
+//    // 게시글 리스트를 출력하는 메소드
+//    public void printNowBoard(ArrayList<BoardVO> boardVOList) {
+//
+//        for(int i = 0; i < boardVOList.size(); i++) {
+//            System.out.printf("%-6s%-12s%-40s%-40s\n",
+//                    boardVOList.get(i).getBno(),
+//                    boardVOList.get(i).getBwriter(),
+//                    boardVOList.get(i).getBdate(),
+//                    boardVOList.get(i).getBtitle());
+//        }
+//    }
+}
